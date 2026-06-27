064bba052248376230ef5429039350af0f075d60
DarioCortezITSON252267 <dario.cortez252267@potros.itson.edu.mx>
2026-06-26 22:39:11 -0700

    Avance 3/4

---
+++ src/main/java/com/example/PRIMESHOP/repository/PedidoRepository.java
@@ -0,0 +1,17 @@
+package com.example.PRIMESHOP.repository;
+
+import Dominio.Pedido;
+import Dominio.enums.EstadoPedido;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+    List<Pedido> findAllByOrderByFechaDesc();
+
+    List<Pedido> findByEstadoOrderByFechaDesc(EstadoPedido estado);
+
+    long countByEstado(EstadoPedido estado);
+}
